c5c0fbf332433ac30cd66e1af39ace67f7196cc5
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-08-31 20:32:53 +0530

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footerDiv'>
+        <div className="socialLinks">
+            <a href="https://twitter.com/CCMOD_"><img src="https://icon.horse/icon/twitter.com" alt="tw"/></a>
+            <a href="https://github.com/Lakshit-Chiranjiv"><img src="https://icon.horse/icon/github.com" alt="gh"/></a>
+            <a href="https://www.linkedin.com/in/lakshit-chiranjiv-sagar-4b0b15198/"><img src="https://icon.horse/icon/www.linkedin.com" alt="li"/></a>
+        </div>
+        <p>Created by Lakshit | All rights reserved | Copyright &copy; {new Date().getFullYear()}</p>
+    </div>
   )
 }
 
