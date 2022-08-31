e5b005bb8e3287948de12ea9dd668f7025ec07d2
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-01 00:45:34 +0530

    added deposit page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import Footer from './components/Footer'
 import Homehero from './components/Homehero'
 import Navbar from './components/Navbar'
+import DepositPage from './pages/DepositPage'
 
 function App() {
 
   return (
     <div className="App">
       <Navbar/>
-      <Homehero/>
+      {/* <Homehero/> */}
+      <DepositPage/>
       <Footer/>
     </div>
   )
