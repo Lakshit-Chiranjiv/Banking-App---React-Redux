ae107330641bac99752d092c9fbbfb8879cecd06
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-01 14:06:44 +0530

    added home wiring on head to home btn using link

--- src/components/HomeButton.jsx
+++ src/components/HomeButton.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Group,Button } from '@mantine/core'
+import { Link } from 'react-router-dom'
 
 const HomeButton = () => {
   return (
     <Group position='center' style={{margin: '24px auto'}}>
-        <Button variant="gradient" gradient={{ from: 'indigo', to: 'cyan' }}>Head To Home</Button>
+        <Link to='/'>
+          <Button variant="gradient" gradient={{ from: 'indigo', to: 'cyan' }}>Head To Home</Button>
+        </Link>
     </Group>
   )
 }
