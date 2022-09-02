58b994a6a5905b0e16fe28fb57361efef5a0c4f9
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-02 23:37:53 +0530

    configured redux thunk middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
-import { createStore } from "redux";
+import { createStore,applyMiddleware } from "redux";
 import allReducers from "./reducers";
+import thunk from "redux-thunk";
 
 export const store = createStore(
     allReducers,    //combined reducers
-    {}  //default state
+    {},
+    applyMiddleware(thunk)  //default state
 )
