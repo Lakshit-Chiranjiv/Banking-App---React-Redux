3068bbf8d7bd9101c8e1a89f3bc42fe64fe27d15
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-09-02 22:50:05 +0530

    rendered actual amount and loan from store

--- src/components/MoneyData.jsx
+++ src/components/MoneyData.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Group,Title } from '@mantine/core'
+import { useSelector } from 'react-redux'
 
 const MoneyData = () => {
+
+  const { amount,loan } = useSelector((state)=> state) 
+
   return (
     <>
       <Group position='center'>
           <Title order={2}>Current Amount in Bank : </Title>
-          <Title order={2} color='green.5'>₹500</Title>
+          <Title order={2} color='green.5'>₹{amount}</Title>
       </Group>
       <Group position='center'>
           <Title order={2}>Current Loan Due : </Title>
-          <Title order={2} color='red.5'>₹34</Title>
+          <Title order={2} color='red.5'>₹{loan}</Title>
       </Group>
     </>
   )
